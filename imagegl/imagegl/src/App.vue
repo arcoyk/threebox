19e1007111f4ecd7b91b7ee96c5e571d6e0a6af4
yui <yuikita21@gmail.com>
2018-11-02 21:27:35 +0900

    quaternion rotate

--- imagegl/imagegl/src/App.vue
+++ imagegl/imagegl/src/App.vue
@@ -44,7 +44,6 @@
 
       const loader = new THREE.TextureLoader();
       const material = new THREE.MeshBasicMaterial({
-        // map: loader.load('https://s3.amazonaws.com/duhaime/blog/tsne-webgl/data/100-img-atlas.jpg')
         map: loader.load('../static/100-img-atlas.jpg')
       });
       var imageSize = {width: 128, height: 128}
@@ -151,10 +150,14 @@
       this.scene.add( this.camera );
       this.scene.add( this.light);
       this.scene.add( this.mesh );
-      this.scene.add( new THREE.FaceNormalsHelper( this.mesh, 20000, 0x00ff00, 11 ) );
       this.controls = new TrackballControls( this.camera, this.renderBox );
-      // document.addEventListener('mousedown', this.onMouseDown, false)
-      setInterval(this.animateCameraPos, 5000)
+      // document.addEventListener('mouseup', this.onMouseDown, false)
+      // setInterval(this.animateCameraPos, 5000)
+      this.camera.lookAt(new THREE.Vector3(1000, 0, 0))
+      this.qa_right = (new THREE.Quaternion()).copy(this.camera.quaternion)
+      this.camera.lookAt(new THREE.Vector3(-1000, 0, 0))
+      this.qa_left = (new THREE.Quaternion()).copy(this.camera.quaternion)
+      setInterval(this.animateCameraLook, 1000)
     },
 
     mounted () {
@@ -176,24 +179,57 @@
         this.raycaster.setFromCamera(this.mouse, this.camera)
         var ins = this.raycaster.intersectObjects( this.scene.children )
         if (ins.length > 0) {
-          // var p = ins[0].point
+
+
+          var p = ins[0].point
+
+          var geometry2 = new THREE.BoxBufferGeometry( 100, 100, 100 );
+          var material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+          var mesh2 = new THREE.Mesh( geometry2, material2 );
+          this.scene.add( mesh2 );
+          mesh2.position.set(p.x, p.y, p.z)
+          console.log(mesh2.position)
+          this.animateCameraPos(p)
           // this.camera.position.setX(p.x)
           // this.camera.position.setY(p.y)
           // this.camera.position.setZ(p.z + 100)
           // this.animateCameraPos(0, 0, Math.random() * 4000)
         }
       },
+
+      animateCameraLook() {
+        const that = this
+        console.log(that.qa)
+        const eye = this.camera.quaternion
+        that.tweenFlag = true
+        that.controls.enabled = false
+        const {autoPlay, Easing, onTick, Tween} = TWEEN;
+        autoPlay(true)
+
+        that.qa = that.qa || that.qa_left
+        if (that.qa._y == -0.5969305296404493) {
+          that.qa = that.qa_left
+        } else {
+          that.qa = that.qa_right
+        }
+
+        const tween = new TWEEN.Tween(eye)
+          .to(that.qa, 1000)
+          .easing(Easing.Cubic.Out) // 最後で減速する
+          .start();
+      },
+
       
-      animateCameraPos(x,y,z) {
-        z = 5000
-        y = (Math.random() - Math.random()) * 4000
-        x = (Math.random() - Math.random()) * 4000
+      animateCameraPos(tar) {
+        // z = 5000
+        // y = (Math.random() - Math.random()) * 4000
+        // x = (Math.random() - Math.random()) * 4000
         const that = this;
         // const coords = new THREE.Vector3()
         const coords = this.camera.position
         // const coords = new THREE.Vector3().copy(this.camera.position)
         // coords.copy(this.camera.position); // 現在地点
-        const destinationVector = new THREE.Vector3(x, y, z); // 終着地点
+        const destinationVector = tar; // 終着地点
         const DURATION = 500;
         that.tweenFlag = true
         that.zeroVector = new THREE.Vector3()
@@ -210,7 +246,7 @@
           .start();
 
         function update() {
-          that.camera.lookAt(that.zeroVector); // 向きを変えながら移動
+          // that.camera.lookAt(that.zeroVector); // 向きを変えながら移動
         }
         // 終着地点に着き次第、requestAnimationFrameをストップ
         function complete() {
