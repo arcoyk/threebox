3d6bf3b18343b8718dc12c5483fb55e3b7482b76
yui <yuikita21@gmail.com>
2018-11-06 14:38:10 +0900

    fraction

--- webgl/webgl/src/App.vue
+++ webgl/webgl/src/App.vue
@@ -10,9 +10,24 @@ import * as SHADER from './js/shader'
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      plane: {},
+      r: 1.0
+    }
+  },
   mounted: function() {
-    const w = 400
-    const h = 400
+    let that = this
+    function onMouseMove( e ) {
+      that.r = (e.clientX / 627) * 2 * Math.PI
+      // that.plane.geometry.uniforms.r.value = that.r
+      that.plane.material.uniforms.r.value = that.r
+    }
+    document.addEventListener( 'mousemove', onMouseMove )
+    let w = 2000
+    let h = 1000
+    w /= 2.6
+    h /= 1.6
 
     // == Scene ==
     let scene = new THREE.Scene()
@@ -24,20 +39,90 @@ export default {
       0.1,
       50
     )
-    camera.position.z = 30
+    camera.position.z = 10
 
     // == Renderer ==
     var renderer = new THREE.WebGLRenderer()
     renderer.setSize( w, h )
 
-    // == Object ==
-    let g = new THREE.PlaneGeometry(10, 10, 32)
-    var m = new THREE.ShaderMaterial({
+    // == Geometry ==
+    let g = new THREE.BufferGeometry()
+    let vs = []
+    const N = 1000
+    for (var i = 0; i < N; i++) {
+      let offset = {
+        x: (Math.random() - Math.random()) * 5,
+        y: (Math.random() - Math.random()) * 5,
+        z: (Math.random() - Math.random()) * 5
+      }
+      vs.push(0.0 + offset.x)
+      vs.push(0.0 + offset.y)
+      vs.push(0.0 + offset.z)
+
+      vs.push((Math.random() - Math.random()) + offset.x)
+      vs.push((Math.random() - Math.random()) + offset.y)
+      vs.push((Math.random() - Math.random()) + offset.z)
+
+      vs.push((Math.random() - Math.random()) + offset.x)
+      vs.push((Math.random() - Math.random()) + offset.y)
+      vs.push((Math.random() - Math.random()) + offset.z)
+    }
+
+    let vertices = new Float32Array([
+      -0.5, -0.5, 0.0,
+       0.5, -0.5, 0.0,
+       0.5,  0.5, 0.0,
+      -0.5,  0.5, 0.0
+    ])
+
+    vertices = new Float32Array(vs)
+
+    let ds = []
+    for (var i = 0; i < 3 * N; i++) {
+      ds.push(i);
+    }
+
+    let indices = new Uint16Array([
+      0, 1, 3,
+      2, 3, 1,
+      3, 1, 0,
+      1, 3, 2
+    ])
+
+    indices = new Uint16Array(ds)
+
+    let cs = []
+    for (var i = 0; i < 3 * N; i++) {
+      // cs.push(i / (3 * N))
+      cs.push(1.0)
+      cs.push(0.0)
+      cs.push(0.0)
+      cs.push(1.0)
+    }
+
+    let colors = new Float32Array([
+      1.0, 0.0, 0.0, 1.0,
+      1.0, 0.0, 0.0, 1.0,
+      1.0, 0.0, 0.0, 1.0
+    ])
+
+    colors = new Float32Array(cs)
+
+    g.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) )
+    g.setIndex( new THREE.BufferAttribute( indices, 1) )
+    g.addAttribute( 'color', new THREE.BufferAttribute( colors, 3) )
+
+    // == Material == 
+    let m = new THREE.ShaderMaterial({
+      uniforms: {
+        r: { value: this.r }
+      },
       vertexShader: SHADER.vshader,
       fragmentShader: SHADER.fshader
     })
-    let plane = new THREE.Mesh(g, m)
-    scene.add(plane)
+    
+    this.plane = new THREE.Mesh(g, m)
+    scene.add(this.plane)
 
     // == Create Canvas ==
     let canvas = document.querySelector( 'canvas' )
@@ -49,8 +134,8 @@ export default {
     // == Render ==
     var render = function() {
       requestAnimationFrame( render )
-      plane.rotation.x += 0.05
-      plane.rotation.y += 0.005
+      // plane.rotation.x += 0.005
+      // plane.rotation.y += 0.005
       renderer.render( scene, camera )
     }
     render()
--- webgl/webgl/src/js/shader.js
+++ webgl/webgl/src/js/shader.js
@@ -1,10 +1,25 @@
 export const vshader = `
+attribute vec4 color;
+varying vec4 varying_color;
+uniform float r;
 void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  mat4 s = mat4(vec4(1.0, 0.0, 0.0, 0.0),
+                vec4(0.0, 1.0, 0.0, 0.0),
+                vec4(0.0, 0.0, 1.0, 0.0),
+                vec4(0.0, 0.0, 0.0, 1.0));
+
+  mat4 rot = mat4(vec4(cos(r), -sin(r), 0, 0),
+                  vec4(sin(r),  cos(r), 0, 0),
+                  vec4(     0,       0, 1, 0),
+                  vec4(     0,       0, 0, 1));
+
+  gl_Position = projectionMatrix * modelViewMatrix * rot * vec4(position, 1.0);
+  varying_color = color;
 }
 `
 export const fshader = `
+varying vec4 varying_color;
 void main() {
-  gl_FragColor = vec4( 0.0, 1.0, 1.0, 1.0 );
+  gl_FragColor = varying_color;
 }
 `
